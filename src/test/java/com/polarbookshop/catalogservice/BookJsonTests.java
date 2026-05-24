d0a03676136c92a17d46ac29aef4ec1017e8a0ec
Aymanhamad2002 <aymanhamad200@gmail.com>
2026-05-24 16:28:50 +0300

    changing the commit stage deployment file and update the tests

--- src/test/java/com/polarbookshop/catalogservice/BookJsonTests.java
+++ src/test/java/com/polarbookshop/catalogservice/BookJsonTests.java
@@ -3,6 +3,7 @@ package com.polarbookshop.catalogservice;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.json.JsonTest;
 import org.springframework.boot.test.json.JacksonTester;
 
@@ -10,33 +11,43 @@ import com.polarbookshop.catalogservice.domain.Book;
 
 @JsonTest
 public class BookJsonTests {
+
+    @Autowired
     private JacksonTester<Book> json;
 
     @Test
-    void testSerialize() throws Exception{
-        var book = new Book("1234567890","Title","Author",9.0);
-        var jsonContent = json.write(book);
-        assertThat(jsonContent).extractingJsonPathStringValue("@.isbn")
-        .isEqualTo(book.isbn());
-        assertThat(jsonContent).extractingJsonPathStringValue("@.title")
-        .isEqualTo(book.title());
-        assertThat(jsonContent).extractingJsonPathStringValue("@.author")
-        .isEqualTo(book.author());
-        assertThat(jsonContent).extractingJsonPathNumberValue("@.price")
-        .isEqualTo(book.price());
+    void testSerialize() throws Exception {
+        var book = new Book("1234567890", "Title", "Author", 9.0);
 
+        var jsonContent = json.write(book);
+
+        assertThat(jsonContent).extractingJsonPathStringValue("@.isbn")
+                .isEqualTo(book.isbn());
+
+        assertThat(jsonContent).extractingJsonPathStringValue("@.title")
+                .isEqualTo(book.title());
+
+        assertThat(jsonContent).extractingJsonPathStringValue("@.author")
+                .isEqualTo(book.author());
+
+        assertThat(jsonContent).extractingJsonPathNumberValue("@.price")
+                .isEqualTo(book.price());
     }
+
     @Test
-    void testDeserialize() throws Exception{
+    void testDeserialize() throws Exception {
+
         var content = """
-        {
-            "isbn" : "1234567890",
-            "title": "Title",
-            "author" : "Author",
-            "price" : 9.0
-        }
+                {
+                    "isbn": "1234567890",
+                    "title": "Title",
+                    "author": "Author",
+                    "price": 9.0
+                }
                 """;
-        assertThat(json.parse(content)).usingRecursiveComparison().isEqualTo(new Book("1234567890","Title","Author",9.0));
+
+        assertThat(json.parse(content))
+                .usingRecursiveComparison()
+                .isEqualTo(new Book("1234567890", "Title", "Author", 9.0));
     }
-    
 }
